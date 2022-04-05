0365dc54d3247731f0f25e3f81ceb133ac9665c2
doanvantan <97596858+doanvantan@users.noreply.github.com>
2022-04-05 09:32:53 +0700

    Add files via upload

---
+++ src/main/java/com/example/bt1/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.bt1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btncong.setOnClickListener {
+            var kq: Double = (so1.text.toString().toDouble()) + so2.text.toString().toDouble()
+            tvketqua.text = kq.toString()
+        }
+        btntru.setOnClickListener {
+            var kq: Double = (so1.text.toString().toDouble())- so2.text.toString().toDouble()
+            tvketqua.text = kq.toString()
+        }
+        btnnhan.setOnClickListener {
+            var kq: Double = (so1.text.toString().toDouble()) * so2.text.toString().toDouble()
+            tvketqua.text = kq.toString()
+        }
+        btnchia.setOnClickListener {
+            var kq: Double = (so1.text.toString().toDouble()) / so2.text.toString().toDouble()
+            tvketqua.text = kq.toString()
+        }
+    }
+}
